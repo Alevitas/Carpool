2222eea5c5875a2ef15591c368e79c260afb7dfd
Akiva Levitas <akivalevitas@Akivas-MacBook-Air.local>
2017-11-06 15:45:41 -0500

    make custom cell

---
+++ Carpool/EventCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventCell.swift
+//  Carpool
+//
+//  Created by Akiva Levitas on 11/6/17.
+//  Copyright © 2017 Akiva Levitas. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+    @IBOutlet weak var dropOffLabel: UIButton!
+    @IBOutlet weak var pickUpLabel: UIButton!
+    @IBOutlet weak var eventNameLabel: UILabel!
+    
+    @IBAction func onPickUpPressed(_ sender: Any) {
+    }
+    @IBAction func onDropOffPressed(_ sender: Any) {
+    }
+    
+    
+    
+    
+}
