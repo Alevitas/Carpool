877e03cb066b52ee916c1615630dafd675f64351
Gary Skoglind <gary.skoglind@gmail.com>
2017-11-06 16:19:26 -0500

    Add PickUp and DropOff buttons to EventDetailsVC

--- Carpool/EventDetailViewController.swift
+++ Carpool/EventDetailViewController.swift
@@ -15,8 +15,11 @@ class EventDetailViewController: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var pickUpButton: UIButton!
+    @IBOutlet weak var dropOffButton: UIButton!
     
     var trip: Trip!
+    var currentUser: User!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +32,23 @@ class EventDetailViewController: UIViewController {
             timeLabel.text = String(describing: trip.event.time)
             
             locationLabel.text = trip.event.location.description
+            
+            if !trip.pickUp.isClaimed {
+                pickUpButton.backgroundColor = UIColor.red
+            }
+
+            if !trip.dropOff.isClaimed {
+                dropOffButton.backgroundColor = UIColor.red
+            }
         }
     }
     
+    @IBAction func onPickUpButtonPressed(_ sender: Any) {
+        pickUpButton.backgroundColor = UIColor.green
+    }
+    
+    @IBAction func onDropOffButtonPressed(_ sender: Any) {
+        dropOffButton.backgroundColor = UIColor.green
+    }
     
 }
