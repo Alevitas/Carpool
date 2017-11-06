b522d50f41757304cfd3e1545e98312e8149aa66
Gary Skoglind <gary.skoglind@gmail.com>
2017-11-06 15:14:37 -0500

    Change event to trip in EventDetailsVC

--- Carpool/EventDetailViewController.swift
+++ Carpool/EventDetailViewController.swift
@@ -16,17 +16,17 @@ class EventDetailViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     
-    var event: Event!
+    var trip: Trip!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Event received is: \(event)")
+        print("Event received is: \(trip.event)")
 
-        descriptionLabel.text = event.description
+        descriptionLabel.text = trip.event.description
         
-        timeLabel.text = String(describing: event.time)
+        timeLabel.text = String(describing: trip.event.time)
 
-        locationLabel.text = event.location.description
+        locationLabel.text = trip.event.location.description
     }
     
     
