a04c37e7868ae1b70d2dd2e6a171319cbf659916
Akiva Levitas <akivalevitas@Akivas-MacBook-Air.local>
2017-11-06 13:54:17 -0500

    Initial Commit

---
+++ Carpool/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  Carpool
+//
+//  Created by Akiva Levitas on 11/6/17.
+//  Copyright © 2017 Akiva Levitas. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
