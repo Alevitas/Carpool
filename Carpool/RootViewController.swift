a8bb15ad2526a131e411a7e4054ef81dda235c53
Akiva Levitas <akivalevitas@Akivas-MacBook-Air.local>
2017-11-06 15:05:46 -0500

    add function to retrieve events

--- Carpool/RootViewController.swift
+++ Carpool/RootViewController.swift
@@ -17,15 +17,21 @@ class RootViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        API.fetchTripsOnce { (trip) in
+            self.trips = trip
+        }
+        tableView.reloadData()
     }
 
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return trips.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+       
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Events", for: indexPath)
         
