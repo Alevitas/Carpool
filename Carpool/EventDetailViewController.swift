78deb6e83cd7feef3a399424d5ffe8eed8375f01
Gary Skoglind <gary.skoglind@gmail.com>
2017-11-06 14:52:40 -0500

    Add Event Details

--- Carpool/EventDetailViewController.swift
+++ Carpool/EventDetailViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import CarpoolKit
+import CoreLocation
 
 class EventDetailViewController: UIViewController {
     
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    var event: Event!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        descriptionLabel.text = event.description
         
+        timeLabel.text = String(describing: event.time)
+
+        locationLabel.text = event.location.description
     }
     
     
--- Carpool/RootViewController.swift
+++ Carpool/RootViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import CarpoolKit
 
 class RootViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
