70d06b31aea8ba1b4f29e9cc1ddc6f1060197a42
Akiva Levitas <akivalevitas@Akivas-MacBook-Air.local>
2017-11-07 10:43:16 -0500

    add fake location for trip

--- Carpool/AddNewViewController.swift
+++ Carpool/AddNewViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import CarpoolKit
+import MapKit
 
-class AddNewViewController: UIViewController {
+class AddNewViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var descriptionTextView: UITextView!
     
     @IBOutlet weak var datePickerOutlet: UIDatePicker!
@@ -17,20 +18,24 @@ class AddNewViewController: UIViewController {
     @IBOutlet weak var dropOffDriverLabel: UILabel!
     
     @IBOutlet weak var pickUpDriverLabel: UILabel!
+    
+    let locationManager = CLLocationManager()
+    var locationAtual: CLLocation = CLLocation(latitude: -56.6462520, longitude: -36.6462520)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        locationManager.delegate = self
         
     }
     @IBAction func onCancelButtonPressed(_ sender: Any) {
         performSegue(withIdentifier: "UnwindFromAddNew", sender: self)
     }
     @IBAction func onAddButtonPressed(_ sender: Any) {
-        API.createTrip(eventDescription: descriptionTextView.text, eventTime: datePickerOutlet.date, eventLocation: CLLocation()) { (trip) in
+        API.createTrip(eventDescription: descriptionTextView.text, eventTime: datePickerOutlet.date, eventLocation: locationAtual) { (trip) in
             
             
-            performSegue(withIdentifier: "UnwindFromAddNew", sender: self)
+            self.performSegue(withIdentifier: "UnwindFromAddNew", sender: self)
         }
         
     }
