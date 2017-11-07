64b5cac2ba490972d0c5ae5b68ec12f292400af8
Akiva Levitas <akivalevitas@Akivas-MacBook-Air.local>
2017-11-07 12:01:12 -0500

    update error

--- Carpool/TripDetailViewController.swift
+++ Carpool/TripDetailViewController.swift
@@ -67,14 +67,14 @@ class TripDetailViewController: UIViewController {
     func onClaimPickUp(action: UIAlertAction) {
         pickUpButton.backgroundColor = UIColor.green
         API.claimLeg(leg: trip.pickUp, trip: trip) { (error) in
-            print("Error claiming PickUp: \(error)")
+            print("Error claiming PickUp: \(String(describing: error))")
         }
     }
     
     func onClaimDropOff(action: UIAlertAction) {
         dropOffButton.backgroundColor = UIColor.green
         API.claimLeg(leg: trip.dropOff, trip: trip) { (error) in
-            print("Error claiming DropOff: \(error)")
+            print("Error claiming DropOff: \(String(describing: error))")
         }
     }
 }
