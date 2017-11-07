5e78c3d56b93fec6609cbcdcdb316cfb2c67d740
Gary Skoglind <gary.skoglind@gmail.com>
2017-11-07 15:06:05 -0500

    Add signInAnonymously to AppDelegate

--- Carpool/AppDelegate.swift
+++ Carpool/AppDelegate.swift
@@ -17,7 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    FirebaseApp.configure()
+        FirebaseApp.configure()
+        
+        Auth.auth().signInAnonymously { (user, error) in
+            print(error)
+        }
         // Override point for customization after application launch.
         return true
     }
