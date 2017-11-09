14eacf2042ea8652534cc88a26b94018f4a26949
Akiva Levitas <akivalevitas@Akivas-MacBook-Air.local>
2017-11-09 16:57:27 -0500

    bunch of stuff

--- Carpool/SettingsViewController.swift
+++ Carpool/SettingsViewController.swift
@@ -13,21 +13,37 @@ import CarpoolKit
 class SettingsViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var childrenAddedLabel: UILabel!
-    var children: [String] = []
+    var children: [Child] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        
+//        childrenAddedLabel.text = makeListOfChildren(childrenList: )
     }
     @IBAction func usernameTextFieldEnter(_ sender: UITextField) {
     }
     
+    func makeListOfChildren(childrenList: [Child]) -> String {
+        var childNames: [String] = []
+        
+        for child in childrenList {
+            childNames.append(child.name)
+        }
+        return childNames.joined(separator: ", ")
+    }
+    
+    
     @IBAction func addChildrenTextFieldEnter(_ sender: UITextField) {
         if let child = sender.text {
-            API.addChild(name: child) { (<#Result<Child>#>) in
-                <#code#>
+            API.addChild(name: child) { (result) in
+                switch result {
+                case .success(let child):
+                    self.children.append(child)
+                case .failure(let error):
+                    print("Error adding child.")
+                }
             }
         }
     }
