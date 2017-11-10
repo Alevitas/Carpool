8cfa093b1940f13b88f9791f1ee62b15cddc61ec
Akiva Levitas <akivalevitas@Akivas-MacBook-Air.local>
2017-11-10 15:55:16 -0500

    got rid of auto naming

--- Carpool/AppDelegate.swift
+++ Carpool/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         FirebaseCommunity.Database.database().isPersistenceEnabled = true
-        API.set(userFullName: "Garkiva")
+        
         Auth.auth().signInAnonymously { (user, error) in
             print("\(String(describing: error))")
         }
