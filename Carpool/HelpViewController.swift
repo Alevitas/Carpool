c7a7c30aedac79cfc47e1cfa7e98d8654b13f116
Akiva Levitas <akivalevitas@Akivas-MacBook-Air.local>
2017-11-13 11:40:55 -0500

    help screen

---
+++ Carpool/HelpViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HelpViewController.swift
+//  Carpool
+//
+//  Created by Akiva Levitas on 11/13/17.
+//  Copyright © 2017 Akiva Levitas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class HelpViewController: UIViewController {
+    @IBOutlet weak var sectionLabel: UILabel!
+    @IBOutlet weak var infoTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    @IBOutlet weak var newTripButton: UIButton!
+    
+    @IBOutlet weak var modifyExistingPressed: UIButton!
+    @IBOutlet weak var addFriendbutton: UIButton!
+    @IBOutlet weak var carpoolScreenPressed: UIButton!
+}
