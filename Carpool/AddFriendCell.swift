8632f8087097057f89b881450b9b768c333e676f
Akiva Levitas <akivalevitas@Akivas-MacBook-Air.local>
2017-11-17 09:54:39 -0500

    spacing

--- Carpool/AddFriendCell.swift
+++ Carpool/AddFriendCell.swift
@@ -14,18 +14,12 @@ class AddFriendCell: UITableViewCell {
     @IBOutlet weak var friendNameLabel: UILabel!
     @IBOutlet weak var addFriendView: UIView!
     
-    
     override func layoutSubviews() {
         super.layoutSubviews()
-        
         
         addFriendView.layer.cornerRadius = 10
         addFriendView.layer.borderWidth = 0.5
         addFriendView.layer.borderColor = UIColor.black.cgColor
         
-        
     }
-    
-    
-    
 }
